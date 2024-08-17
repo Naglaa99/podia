21f42235abcf836fa87d47a0d735a9aea9728f15
Naglaa99 <Naglaahamdi6@gmail.com>
2024-08-18 00:07:04 +0200

    making website responsive with all small screen

--- javascript/script.js
+++ javascript/script.js
@@ -9,3 +9,16 @@ function stickyHeader() {
     header.classList.remove("sticky");
   }
 }
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  });
+});
